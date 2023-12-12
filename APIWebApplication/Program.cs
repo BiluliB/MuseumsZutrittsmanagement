4162902cbddc81a3b544331c85a32fa6a46e80ba
David Reichert <david.reichert@ict-scouts.ch>
2023-12-12 10:49:41 +0100

    wwwroot removed

--- APIWebApplication/Program.cs
+++ APIWebApplication/Program.cs
@@ -18,7 +18,7 @@ namespace MuseumsZutrittWebApplication
             builder.Services.AddScoped<MuseumAreaService>();
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
