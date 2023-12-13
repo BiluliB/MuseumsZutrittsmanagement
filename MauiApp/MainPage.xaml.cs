dfa22a16ef318c05767f3faf2ad50f661962d16f
Bobby <138381572+BiluliB@users.noreply.github.com>
2023-12-13 07:24:36 +0100

    add button animation and grid

--- MauiApp/MainPage.xaml.cs
+++ MauiApp/MainPage.xaml.cs
@@ -12,5 +12,42 @@ namespace MuseumsZutrittMauiApp
             InitializeComponent();
             BindingContext = new MainPageViewModel();
         }
+        private async Task AnimateButton(Button button)
+        {
+            await Task.WhenAll(
+                button.ScaleTo(0.99, 50, Easing.Linear), // Leicht verkleinern
+                button.FadeTo(0.8, 50, Easing.Linear) // Opazität reduzieren
+            );
+            await Task.WhenAll(
+                button.ScaleTo(1, 50, Easing.Linear), // Zurück zur ursprünglichen Größe
+                button.FadeTo(1, 50, Easing.Linear) // Opazität zurücksetzen
+            );
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await AnimateButton(sender as Button);
+            // Weitere Logik für den Button
+        }
+
+        private async void Button_Pressed(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            await Task.WhenAll(
+                button.ScaleTo(0.99, 50, Easing.Linear), // Leicht verkleinern
+                button.FadeTo(0.8, 50, Easing.Linear) // Opazität reduzieren
+            );
+        }
+
+        private async void Button_Released(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            await Task.WhenAll(
+                button.ScaleTo(1, 50, Easing.Linear), // Zurück zur ursprünglichen Größe
+                button.FadeTo(1, 50, Easing.Linear) // Opazität zurücksetzen
+            );
+        }
+
+
     }
 }
