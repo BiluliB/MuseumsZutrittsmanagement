2808dbb9403082a937d8f8db439768260c85fa74
Bobby <138381572+BiluliB@users.noreply.github.com>
2023-12-15 06:12:56 +0100

    Update MuseumArea.cs

--- APIWebApplication/Models/MuseumArea.cs
+++ APIWebApplication/Models/MuseumArea.cs
@@ -1,5 +1,8 @@
 ﻿namespace APIWebApplication.Models
 {
+    /// <summary>
+    /// Model for Museum area
+    /// </summary>
     public class MuseumArea
     {
         public int Id { get; set; }
@@ -8,7 +11,7 @@
         public string AreaLocation { get; set; }
         public string AccessRules { get; set; }
 
-        // Lazy Loading für die Navigationseigenschaften
+        // Lazy loading for navigation properties
         public virtual ICollection<VisitorCapacity> VisitorCapacities { get; set; }
         public virtual ICollection<OpeningHour> OpeningHours { get; set; }
     }
